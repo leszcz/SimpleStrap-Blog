6f3d458cbfce66f11c94da0950cc4dd9eacb57a3
tomekleszczynski94@gmail.com <leszczynski@s7.sherahost.pl>
2024-01-12 10:01:10 +0100

    first commit

---
+++ style.css
@@ -0,0 +1,9 @@
+/*
+Theme Name: SimpleStrap Blog
+Author: Tomasz Leszczynski <tomasz@leszczynski.space>
+Author URI: https://leszczynski.space
+Description: Simple responsive theme for Wordpress blogs using Bootstrap 5
+Version: 1.0
+License: GNU General Public License v3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
